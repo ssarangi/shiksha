b0462bf692e0c594fb62a4f6bcd45e2fefa32640
Satyajit Sarangi <satyajits@google.com>
2019-07-23 22:19:14 -0700

    Operations generator

--- shiksha_engine/generators/generator.py
+++ shiksha_engine/generators/generator.py
@@ -5,6 +5,7 @@ various math expressions. Other files in this directory are not meant to be expo
 
 from abc import ABC
 import numpy as np
+import sympy
 # from . import algebra
 from . import operators
 
@@ -37,20 +38,93 @@ class SingleSymbolTermGenerator(Generator):
 class OperationsGenerator(Generator):
     def __init__(self, symbol):
         self._symbol = symbol
-    
+        self._constant_generator = ConstantGenerator(0, 10)
+
+    def _method_name_from_operation(self, operation: operators.Operations):
+        method_name = "_" + operation.name.lower()
+        print(OperationsGenerator.__dict__.keys())
+        if method_name not in OperationsGenerator.__dict__.keys():
+            raise Exception("Method %s not implemented" % method_name)
+        return method_name
+
     def generate(self):
         operation = np.random.choice(operators.Operations)
+        method_name = self._method_name_from_operation(operation)
+        method_to_call = getattr(self, method_name)
+        return method_to_call()
 
     def _add_constant(self):
-        pass
+        constant = self._constant_generator.generate()
+        return self._symbol + constant
     
     def _subtract_constant(self):
-        pass
+        constant = self._constant_generator.generate()
+        return self._symbol - constant
 
     def _multiply_constant(self):
-        pass
+        constant = self._constant_generator.generate()
+        return self._symbol * constant
 
     def _divide_constant(self):
-        pass
+        constant = self._constant_generator.generate()
+        return self._symbol / constant
+
+    def _raise_to_the_power(self):
+        constant = self._constant_generator.generate()
+        return self._symbol ** constant
+
+    def _take_log(self):
+        return sympy.log(self._symbol)
+
+    def _add_expr(self, expr):
+        return self._symbol + expr
+
+    def _subtract_expr(self, expr):
+        return self._symbol - expr
+
+    def _multiply_expr(self, expr):
+        return self._symbol * expr
+
+    def _divide_expr(self, expr):
+        return self._symbol / expr
+
+    def _invert(self):
+        return 1 / self._symbol
+
+    def _cos(self):
+        return sympy.cos(self._symbol)
+
+    def _sin(self):
+        return sympy.sin(self._symbol)
+
+    def _tan(self):
+        return sympy.tan(self._symbol)
+
+    def _cosh(self):
+        return sympy.cosh(self._symbol)
+
+    def _acosh(self):
+        return sympy.acosh(self._symbol)
+
+    def _sinh(self):
+        return sympy.sinh(self._symbol)
+
+    def _asinh(self):
+        return sympy.asinh(self._symbol)
+
+    def _tanh(self):
+        return sympy.tanh(self._symbol)
+
+    def _atanh(self):
+        return sympy.atanh(self._symbol)
+
+    def _atan2(self):
+        return sympy.atan2(self._symbol)
+
+    def _cot(self):
+        return sympy.cot(self._symbol)
+
+    def _coth(self):
+        return sympy.coth(self._symbol)
 
     
